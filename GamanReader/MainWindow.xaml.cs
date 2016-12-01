b77317f9f62e3041c701b504c5011f58790c023d
Zoltanar <universegusty@gmail.com>
2016-12-01 01:39:21 +0000

    Initial Commit

--- GamanReader/MainWindow.xaml.cs
+++ GamanReader/MainWindow.xaml.cs
@@ -1,28 +1,181 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace GamanReader
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// WPF application for reading manga with a dual-page view.
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private ReaderViewModel _rvm;
+        private int _pageSize = 2;
+        private bool _rtlDirection; //false for left to right, true for right to left
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadFolder(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new CommonOpenFileDialog { IsFolderPicker = true, AllowNonFileSystemItems = true };
+            var result = folderPicker.ShowDialog();
+            if (result != CommonFileDialogResult.Ok) return;
+            var files = Directory.GetFiles(folderPicker.FileName);
+            if (!files.Any())
+            {
+                ReplyLabel.Content = "No files in folder.";
+                return;
+            }
+            _rvm = new ReaderViewModel(folderPicker.FileName, files);
+            ReplyLabel.Content = _rvm.TotalFiles + " files in folder.";
+            PopulatePreviousBox(_rvm.GetFile);
+            if (_rvm.TotalFiles > 1) PopulateNextBox(_rvm.GetFileForward);
+        }
+
+        private void PopulatePreviousBox(string filename)
+        {
+            Image imagebox = _rtlDirection ? RightImageBox : LeftImageBox;
+            Label label = _rtlDirection ? RightSideLabel : LeftSideLabel;
+            imagebox.Source = new BitmapImage(new Uri(filename));
+            label.Content = $"({_rvm.CurrentIndex}){Path.GetFileName(filename)}";
+        }
+
+
+        private void PopulateNextBox(string filename)
+        {
+            Image imagebox = _rtlDirection ? LeftImageBox : RightImageBox;
+            Label label = _rtlDirection ? LeftSideLabel : RightSideLabel;
+            imagebox.Source = new BitmapImage(new Uri(filename));
+            label.Content = $"({_rvm.CurrentIndex + 1}){Path.GetFileName(filename)}";
+        }
+
+        private void HandleKeys(object sender, KeyEventArgs e)
+        {
+            if (_rvm == null) return;
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (_rtlDirection)
+                    {
+                        if (_rvm.FilesAhead >= 2)
+                        {
+                            GoForward(CtrlIsDown() ? 1 : _pageSize);
+                        }
+                    }
+                    else
+                    {
+                        if (_rvm.HasPrevious)
+                        {
+                            GoBack(CtrlIsDown() ? 1 : _pageSize);
+                        }
+                    }
+                    return;
+                case Key.Right:
+                    if (_rtlDirection)
+                    {
+                        if (_rvm.HasPrevious)
+                        {
+                            GoBack(CtrlIsDown() ? 1 : _pageSize);
+                        }
+                    }
+                    else
+                    {
+                        if (_rvm.FilesAhead >= 2)
+                        {
+                            GoForward(CtrlIsDown() ? 1 : _pageSize);
+                        }
+                    }
+                    return;
+            }
+        }
+
+        public static bool CtrlIsDown()
+        {
+            return Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
+        }
+
+        private void GoBack(int moveNumber)
+        {
+            if (moveNumber == 1)
+            {
+                _rvm.CurrentIndex--;
+            }
+            else if (_rvm.HasTwoPrevious)
+            {
+                _rvm.CurrentIndex -= _pageSize;
+            }
+            else
+            {
+                _rvm.CurrentIndex--;
+            }
+            PopulatePreviousBox(_rvm.GetFile);
+            if (_rvm.HasNext) PopulateNextBox(_rvm.GetFileForward);
+            IndexLabel.Content = $"{_rvm.CurrentIndex + 1}/{_rvm.TotalFiles}";
+
+        }
+        private void GoForward(int moveNumber)
+        {
+            if (moveNumber == 0)
+            {
+                PopulatePreviousBox(_rvm.GetFile);
+                if (_rvm.HasNext) PopulateNextBox(_rvm.GetFileForward);
+            }
+            else if (moveNumber == 1)
+            {
+                _rvm.CurrentIndex++;
+            }
+            else if (_rvm.FilesAhead >= 3)
+            {
+                _rvm.CurrentIndex += _pageSize;
+            }
+            else
+            {
+                _rvm.CurrentIndex++;
+            }
+            PopulatePreviousBox(_rvm.GetFile);
+            if (_rvm.HasNext) PopulateNextBox(_rvm.GetFileForward);
+            IndexLabel.Content = $"{_rvm.CurrentIndex+1}/{_rvm.TotalFiles}";
+
+        }
+
+        private void MakeLtr(object sender, RoutedEventArgs e)
+        {
+            DirectionButton.Content = "▶ Left-to-Right ▶";
+            _rtlDirection = false;
+            GoForward(0);
+        }
+
+        private void MakeRtl(object sender, RoutedEventArgs e)
+        {
+            DirectionButton.Content = "◀ Right-to-Left ◀";
+            _rtlDirection = true;
+            GoForward(0);
+        }
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (_rvm == null) return;
+            if (e.Delta < 0)
+            {
+                if (_rvm.FilesAhead >= 2)
+                {
+                    GoForward(CtrlIsDown() ? 1 : _pageSize);
+                }
+            }
+            else if (e.Delta > 0)
+            {
+                if (_rvm.HasPrevious)
+                {
+                    GoBack(CtrlIsDown() ? 1 : _pageSize);
+                }
+            }
         }
     }
 }
---
+++ GamanReader/ReaderViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace GamanReader
+{
+    /// <summary>
+    /// Model that contains details about currently opened folder/file.
+    /// </summary>
+    internal class ReaderViewModel
+    {
+        public ReaderViewModel(string folderName, string[] fileNames)
+        {
+            FolderName = folderName;
+            CurrentIndex = 0;
+            foreach (var imageCodec in ImageCodecInfo.GetImageEncoders())
+                _recognizedExtensions.AddRange(imageCodec.FilenameExtension.ToLowerInvariant().Split(";".ToCharArray()));
+            FileNames = fileNames.Where(FileIsImage).ToArray();
+            TotalFiles = FileNames.Length;
+        }
+
+        public string FolderName { get; }
+        public int CurrentIndex { get; set; }
+        public int TotalFiles { get; }
+        public string[] FileNames { get; }
+
+
+        public int FilesAhead => TotalFiles - (CurrentIndex + 1);
+        public bool HasNext => TotalFiles - (CurrentIndex + 1) > 0;
+        public bool HasPrevious => CurrentIndex > 0;
+        public bool HasTwoPrevious => CurrentIndex > 1;
+        public string GetFile => FileNames[CurrentIndex];
+        public string GetFileForward => FileNames[CurrentIndex + 1];
+
+        private readonly List<string> _recognizedExtensions = new List<string>();
+        private bool FileIsImage(string filename)
+        {
+            var ext = Path.GetExtension(filename);
+            return _recognizedExtensions.Exists(x =>
+            {
+                var extension = Path.GetExtension(x);
+                return extension != null && extension.Equals(ext);
+            });
+        }
+    }
+}
